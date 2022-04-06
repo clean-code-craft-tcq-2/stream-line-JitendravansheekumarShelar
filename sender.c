092c4d66d8cad96afc40abd8d5b8d5e5ed057dd9
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-06 22:30:21 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -2,10 +2,48 @@
 #include <string.h>
 #include "sender.h"
 
+// Declare the file pointer
+FILE *filePointer ;
+
+// Open the existing file SensorData.csv using fopen()
+// in read mode using "r" attribute
+filePointer = fopen("SensorData.csv", "r") ;
+
+int isFileOpenSuccessfully()
+{
+    // Check if this filePointer is null
+    // which maybe if the file does not exist
+    if ( filePointer == NULL )
+    {
+        printf( "SensorData.csv file failed to open." ) ;
+        return 0;
+    }
+    else
+    {
+         
+        printf("The file is now opened.\n") ;
+        return 1;
+    }
+}
+
+int isDataReadSuccessfully()
+{
+    reutrn 1;
+}
+
+int readData()
+{
+    return 50;
+}
 
 int m_readDataFromAFile()
-{
-    int totalReadingsCaptured = 0;
-    
-    return totalReadingsCaptured;
+{   
+    if(isFileOpenSuccessfully())
+    {
+        return readData();
+    }
+    else
+    {
+        return 0;
+    }
 }
