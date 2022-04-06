bd195fd2d9ff3d10840c3895fb18e07feb52018f
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-06 23:44:01 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -8,6 +8,27 @@ using namespace std;
 
 TEST_CASE("Read Data from a File"){
   int totalReadingsCaptured = 0;
+  char *filename = 'Sensor1_Data.csv'
   totalReadingsCaptured = m_readDataFromAFile();
   REQUIRE (totalReadingsCaptured == 50);
+  }
+
+TEST_CASE("Whether data read successfully"){
+  int isReadingSuccess = 0;
+  isReadingSuccess = isDataReadSuccessfully();
+  REQUIRE (isReadingSuccess == 1);
+  }
+
+
+TEST_CASE("Read Data from a not present File"){
+  int totalReadingsCaptured = 0;
+  char *filename = 'SensorData.csv'
+  totalReadingsCaptured = m_readDataFromAFile(filename);
+  REQUIRE (totalReadingsCaptured == 0);
+  }
+
+TEST_CASE("When data is not available"){
+  int isReadingSuccess = 0;
+  isReadingSuccess = isDataReadSuccessfully();
+  REQUIRE (isReadingSuccess == 0);
   }
