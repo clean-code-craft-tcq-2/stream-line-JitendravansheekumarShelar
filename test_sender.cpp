b890e948001f4cf68728cbbbf4161208c0f1d546
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-06 23:55:29 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -2,20 +2,21 @@
 
 #include "test/catch.hpp"
 #include "sender.h"
+#include <string.h>
 
 using namespace std;
 
 //Positive Scenarios
 TEST_CASE("Read Data from a File"){
   int totalReadingsCaptured = 0;
-  char *filename = 'Sensor1_Data.csv'
+  char *filename = "Sensor1_Data.csv";
   totalReadingsCaptured = m_readDataFromAFile(filename);
   REQUIRE (totalReadingsCaptured == 50);
   }
 
 TEST_CASE("Whether data read successfully"){
   int isReadingSuccess = 0;
-  char *filename = 'Sensor1_Data.csv
+  char *filename = "Sensor1_Data.csv";
   isReadingSuccess = isDataReadSuccessfully(filename);
   REQUIRE (isReadingSuccess == 1);
   }
@@ -23,14 +24,14 @@ TEST_CASE("Whether data read successfully"){
 //Negative Scenarios
 TEST_CASE("Read Data from a not present File"){
   int totalReadingsCaptured = 0;
-  char *filename = 'SensorData.csv'
+  char *filename = "SensorData.csv";
   totalReadingsCaptured = m_readDataFromAFile(filename);
   REQUIRE (totalReadingsCaptured == 0);
   }
 
 TEST_CASE("When data is not available"){
   int isReadingSuccess = 0;
-  char *filename = 'SensorData.csv'
+  char *filename = "SensorData.csv";
   isReadingSuccess = isDataReadSuccessfully(filename);
   REQUIRE (isReadingSuccess == 0);
   }
