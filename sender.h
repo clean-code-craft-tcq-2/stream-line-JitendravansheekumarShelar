68c11d882e12e497826af4be4355f8d82434e98c
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-07 00:02:21 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -1,1 +1,5 @@
-int m_readDataFromAFile();
+extern int readData();
+extern int m_readDataFromAFile(char *filename);
+extern int isFileOpenSuccessfully(char *filename);
+extern int isDataReadSuccessfully(char *filename);
+
