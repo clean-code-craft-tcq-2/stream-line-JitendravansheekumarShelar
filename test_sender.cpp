78638215d82a13177eedb91d8f1b474cf6230dc2
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-07 17:57:33 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -16,13 +16,6 @@ TEST_CASE("Read Data from a File"){
   REQUIRE (totalReadingsCaptured == 50);
   }
 
-/*TEST_CASE("Whether data read successfully"){
-  int isReadingSuccess = 0;
-  char *filename = "./Sensor1_Data.txt";
-  isReadingSuccess = isDataReadSuccessfully(filename);
-  REQUIRE (isReadingSuccess == 1);
-  }*/
-
 //Negative Scenarios
 TEST_CASE("Read Data from a not present File"){
   int totalReadingsCaptured = 0;
@@ -33,11 +26,3 @@ TEST_CASE("Read Data from a not present File"){
   REQUIRE (totalReadingsCaptured == 0);
   }
 
-/*TEST_CASE("When data is not available"){
-  int isReadingSuccess = 0;
-  float Temperature[50]={0};
-  float ChargeRate[50]={0};
-  char *filename = "./SensorData.txt";
-  isReadingSuccess = isDataReadSuccessfully(filename,&Temperature[0],&ChargeRate[0]);
-  REQUIRE (isReadingSuccess == 0);
-  }*/
