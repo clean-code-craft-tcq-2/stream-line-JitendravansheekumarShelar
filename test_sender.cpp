115010c08acc2fcdaaf90ae2a8752ef6811cf596
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-07 14:35:43 +0530

    Update test_sender.cpp

--- test_sender.cpp
+++ test_sender.cpp
@@ -9,29 +9,33 @@ using namespace std;
 //Positive Scenarios
 TEST_CASE("Read Data from a File"){
   int totalReadingsCaptured = 0;
+  float Temperature;
+  float ChargeRate;
   char *filename = "./Sensor1_Data.txt";
-  totalReadingsCaptured = m_readDataFromAFile(filename);
+  totalReadingsCaptured = m_readDataFromAFile(filename,&Temperature,&ChargeRate);
   REQUIRE (totalReadingsCaptured == 50);
   }
 
-TEST_CASE("Whether data read successfully"){
+/*TEST_CASE("Whether data read successfully"){
   int isReadingSuccess = 0;
   char *filename = "./Sensor1_Data.txt";
   isReadingSuccess = isDataReadSuccessfully(filename);
   REQUIRE (isReadingSuccess == 1);
-  }
+  }*/
 
 //Negative Scenarios
 TEST_CASE("Read Data from a not present File"){
   int totalReadingsCaptured = 0;
+  float Temperature;
+  float ChargeRate;
   char *filename = "./SensorData.txt";
-  totalReadingsCaptured = m_readDataFromAFile(filename);
+  totalReadingsCaptured = m_readDataFromAFile(filename,&Temperature,&ChargeRate);
   REQUIRE (totalReadingsCaptured == 0);
   }
 
-TEST_CASE("When data is not available"){
+/*TEST_CASE("When data is not available"){
   int isReadingSuccess = 0;
   char *filename = "./SensorData.txt";
   isReadingSuccess = isDataReadSuccessfully(filename);
   REQUIRE (isReadingSuccess == 0);
-  }
+  }*/
