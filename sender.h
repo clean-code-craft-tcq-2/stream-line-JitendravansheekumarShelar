27a562443ebd9763a112d86f57be0d6fa1c19403
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-07 18:01:37 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -1,3 +1,7 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 extern int readData(FILE *filePointer, float *Temperature, float *ChargeRate);
 extern int m_readDataFromAFile(char *filename, float *Temperature, float *ChargeRate);
 extern int isFileOpenSuccessfully(FILE *filePointer);
