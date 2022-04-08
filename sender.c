9b14f16a6fe6b71eb71def503ad25d72d3cbe082
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-09 00:26:24 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -5,37 +5,6 @@
 
 #define BUFFER_SIZE 50
 
-int isFileOpenSuccessfully(FILE *filePointer)
-{    
-    // Check if this filePointer is null
-    // which maybe if the file does not exist
-    return ( filePointer == NULL ) ? 0 : 1;
-}
-
-int sendDataToConsole( float *Temperature, float *ChargeRate)
-{
-    /* Every program you may run on the command line has 3 streams, STDIN, STDOUT and STDERR */
-    fprintf(stdin, "%f , %f\n",Temperature,ChargeRate);    
-    return BUFFER_SIZE;
-}
-
-int readDataAndSendToConsole(FILE *filePointer, float *Temperature, float *ChargeRate)
-{
-    float Temp_data = 0.0, ChargeRate_data = 0.0;
-    int index_pos;
-  
-    for(index_pos=0; index_pos<BUFFER_SIZE; index_pos++)
-    {
-      fscanf(filePointer, "%f , %f \n", &Temp_data,&ChargeRate_data);
-      *(Temperature + index_pos) = Temp_data;
-      *(ChargeRate + index_pos) = ChargeRate_data;
-    }
-    
-    fclose(filePointer);
-    
-    return sendDataToConsole(Temperature,ChargeRate);
-}
-
 int accessFileForDataExtraction(char * filename, float *Temperature, float *ChargeRate)
 {  
     // Declare the file pointer
@@ -45,5 +14,35 @@ int accessFileForDataExtraction(char * filename, float *Temperature, float *Char
     // in read mode using "r" attribute
     filePointer = fopen(filename, "r");
     
-    return isFileOpenSuccessfully(filePointer) ? readData(filePointer,Temperature,ChargeRate) : 0;
+    return isFileOpenSuccessfully(filePointer) ? readDataAndSendToConsole(filePointer,Temperature,ChargeRate) : 0;
 }
+
+int isFileOpenSuccessfully(FILE *filePointer)
+{    
+    // Check if this filePointer is null
+    // which maybe if the file does not exist
+    return ( filePointer == NULL ) ? 0 : 1;
+}
+
+int readDataAndSendToConsole(FILE *filePointer, float *Temperature, float *ChargeRate)
+{
+    float Temp_data = 0.0, ChargeRate_data = 0.0;
+    int index_pos;  
+    for(index_pos=0; index_pos<BUFFER_SIZE; index_pos++)
+    {
+      fscanf(filePointer, "%f , %f \n", &Temp_data,&ChargeRate_data);
+      *(Temperature + index_pos) = Temp_data;
+      *(ChargeRate + index_pos) = ChargeRate_data;
+    }
+    fclose(filePointer);    
+    return sendDataToConsole(Temperature,ChargeRate);
+}
+
+int sendDataToConsole( float *Temperature, float *ChargeRate)
+{
+    /* Every program you may run on the command line has 3 streams, STDIN, STDOUT and STDERR */
+    fprintf(stdin, "%f , %f\n",Temperature,ChargeRate);    
+    return BUFFER_SIZE;
+}
+
+
