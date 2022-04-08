a2b89b7bcdb6f8687fdb2cafdd993def8337b602
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-09 00:22:44 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -2,8 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int readData(FILE *filePointer, float *Temperature, float *ChargeRate);
-extern int sendDataToConsole( float *Temperature, float *ChargeRate);
+//Requirement - 1
 extern int accessFileForDataExtraction(char *filename, float *Temperature, float *ChargeRate);
+//Requirement - 2
 extern int isFileOpenSuccessfully(FILE *filePointer);
+//Requirement - 3
+extern int readDataAndSendToConsole(FILE *filePointer, float *Temperature, float *ChargeRate);
+//Requirement - 4
+extern int sendDataToConsole(float *Temperature, float *ChargeRate);
 
