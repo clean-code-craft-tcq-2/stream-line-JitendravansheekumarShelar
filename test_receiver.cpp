8d0f60114c7e4af38dcbd752ef5301c298c3a55f
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-13 13:43:05 +0530

    Update test_receiver.cpp

--- test_receiver.cpp
+++ test_receiver.cpp
@@ -10,14 +10,14 @@ TEST_CASE("Test for Receiever to read from console")
   float chargeRate[6];
   float expectedTempValue[6] = {35, 33, 49, 46, 30, 37};
   float expectedChargeRate[6] = {55, 60, 58, 52, 59, 51};
-  printf("Print the data on console\n");
+  //printf("Print the data on console\n");
   for(int i = 0; i<6; i++){
-    printf(" Temperature: %.2f, Charge Rate: %.2f \n", expectedTempValue[i], expectedChargeRate[i]);
+    printf("Temperature: %.2f, Charge Rate: %.2f \n", expectedTempValue[i], expectedChargeRate[i]);
   }
   receiveDataFromConsole(temp,chargeRate);  
   printf("Print the received data from console\n");
   for(int i = 0; i<6; i++){
-    printf(" Temperature: %.2f, Charge Rate: %.2f \n", temp[i], chargeRate[i]);
+    printf("Temperature: %.2f, Charge Rate: %.2f \n", temp[i], chargeRate[i]);
   }
   
 }
