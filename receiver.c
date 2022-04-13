7b31bf802cf05625d81a4dc46d3af44dc74b84c1
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-13 11:38:54 +0530

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "receiver.h"
+
+#define BUFFER_SIZE 50
+
+// Here we consider that the output printed on console (By sender) is of the type
+// Temperature: 31.342, Charge Rate: 0.413
+void receiveDataFromConsole(float* tempData, float* chargeRateData) {
+    char unusedData[300];
+    for(int j = 0; j < BUFFER_SIZE; j++){
+        if(scanf("%50s", unusedData) == EOF) 
+        {
+            break;
+        } 
+        scanf("%f", &temperature[j]);
+        scanf("%50s", unusedData);
+        scanf("%50s", unusedData);
+        scanf("%50s", unusedData);
+        scanf("%f", &chargeRate[j]);
+        printf("%f, %f, %f\n", temperature[j], chargeRate[i]);        
+    }
+}
