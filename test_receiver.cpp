51d024cf8f4ca6f78351fc1f91b4f43e07810ddd
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-14 14:19:55 +0530

    Update test_receiver.cpp

--- test_receiver.cpp
+++ test_receiver.cpp
@@ -12,8 +12,8 @@ TEST_CASE("Test for Receiever to read from console")
   receiveDataFromConsole(temp,chargeRate);                                                              
   float expectedTempValue[50] = {35, 33, 49, 46};
   float expectedChargeRate[50] = {55, 60, 58, 52};
-       
+  for(int i =0; i< 4; i++){
   REQUIRE( (temp[i] - expectedTempValue[i]) <= 0.001);
   REQUIRE( (chargeRate[i] - expectedChargeRate[i]) <= 0.001);
-
+  }
 }
