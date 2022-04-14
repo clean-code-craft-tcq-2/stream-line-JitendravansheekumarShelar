011fb1730fe3ff9e8576644bbc39ba767a1dd9f2
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-14 17:02:24 +0530

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -7,6 +7,25 @@
 
 #define BUFFER_SIZE 50
 
+void printMinMaxValuesInStream(float* tempData, float* chargeRateData) {
+    float minTemp=0, maxTemp=0, minChargeRate=0, maxChargeRate=0;
+    for (int j = 0; j < BUFFER_SIZE; j++)
+    {
+        if(tempData[j] < minTemp) {
+            minTemp = tempData[j];
+        } else if(temperature[j]> maxTemp) {
+            maxTemp = tempData[j];
+        }
+
+        if(chargeRateData[j] < minChargeRate) {
+            minChargeRate = chargeRateData[j];
+        } else if(stateOfCharge[j] > maxChargeRate) {
+            maxChargeRate = chargeRateData[j];
+        }
+    }
+    printf("Minimum Temperature in the stream: %f Maximum Temperature in the stream: %f Minimum Charge Rate in the stream: %f Maximum Charge Rate in the stream: %f\n", minTemp, maxTemp, minChargeRate, maxChargeRate);
+}
+
 // Here we consider that the output printed on console (By sender) is of the type
 // Temperature: 31.342, Charge Rate: 0.413
 void receiveDataFromConsole(float* tempData, float* chargeRateData) {
