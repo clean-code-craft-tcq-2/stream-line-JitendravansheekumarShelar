275eb302fc5d8eafd7b7dba5ab672b50bebbabe0
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-14 14:54:58 +0530

    Update test_receiver.cpp

--- test_receiver.cpp
+++ test_receiver.cpp
@@ -8,7 +8,6 @@
 TEST_CASE("Test for Receiever to read from console") 
 {
   float temp[50], chargeRate[50]  = {0};
-  float Temperature, chargeRateFromFile;
   receiveDataFromConsole(temp,chargeRate);                                                              
   float expectedTempValue[50] = {35, 33, 49, 46};
   float expectedChargeRate[50] = {55, 60, 58, 52};
@@ -24,17 +23,16 @@ TEST_CASE("Test for Receiever to read from console and validate with File conten
   float Temperature, chargeRateFromFile;
   receiveDataFromConsole(temp,chargeRate);                                                              
 
-   FILE * file= fopen("./Sensor1_Data.txt","r");                                                                  
-     if (file!=NULL) 
-     {
-        for(int i=0;fscanf(file, "%f\t%f\n", &Temperature,&chargeRateFromFile)!=EOF ;i++)
+  FILE * file= fopen("./Sensor1_Data.txt","r");                                                                  
+  if (file!=NULL) 
+  {
+     for(int i=0;fscanf(file, "%f\t%f\n", &Temperature,&chargeRateFromFile)!=EOF ;i++)
         {
             expectedTempValue[i] = Temperature;
             expectedChargeRate[i] = chargeRateFromFile;
+            REQUIRE( (temp[i] - expectedTempValue[i]) <= 0.001);
+            REQUIRE( (chargeRate[i] - expectedChargeRate[i]) <= 0.001);
         }
-       
-     REQUIRE( (temp[i] - expectedTempValue[i]) <= 0.001);
-     REQUIRE( (chargeRate[i] - expectedChargeRate[i]) <= 0.001);
    }
    fclose(file);
 }
